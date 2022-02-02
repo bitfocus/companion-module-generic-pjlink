2c172cae98ae6f3781b2a992fe3c137f792c4bdb
Julian Waller <git@julusian.co.uk>
2022-02-02 16:43:29 +0000

    bulk replace system.emit calls

--- pjlink.js
+++ pjlink.js
@@ -228,7 +228,7 @@ instance.prototype.destroy = function() {
 instance.prototype.actions = function(system) {
 	var self = this;
 
-	self.system.emit('instance_actions', self.id, {
+	self.setActions({
 		'powerOn':        { label: 'Power On Projector' },
 		'powerOff':       { label: 'Power Off Projector' },
 		'shutterOpen':    { label: 'Open Shutter' },
