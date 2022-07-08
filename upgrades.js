b0cd729ec600201a5f25da56317aadd57af96e64
John A Knight Jr <istnv@ayesti.com>
2022-07-07 22:42:57 -0700

    Add split A and V options to mute

--- upgrades.js
+++ upgrades.js
@@ -61,8 +61,38 @@ module.exports = {
 		let changed = false
 
 		actions.forEach((action) => {
-			if ('muteState' == action.action && action.options.opt.slice(0,1)=='3') {
-				action.options.opt = action.options.opt.slice(1,2)
+			// if ('muteState' == action.action && action.options.opt.slice(0,1)=='3') {
+			// 	action.options.opt = action.options.opt.slice(1,2)
+			// 	changed = true
+			// }
+		})
+		return changed
+	},
+	upgrade_muteaction2: function (context, config, actions, feedbacks) {
+		let changed = false
+
+		actions.forEach((action) => {
+			if ('muteState' == action.action) {
+				if (action.options.opt.length == 2) {
+					action.options.opt = action.options.opt.slice(1,2)
+					action.options.item = action.options.opt.slice(0,1)
+					changed = true
+				} else if (action.options.item == undefined) {
+					action.options.item = '3'
+					changed = true
+				}
+			}
+		})
+
+
+		feedbacks.forEach((fb) => {
+			if ('muteState' == fb.type) {
+				if (fb.options.muteState) {
+					fb.options.item = fb.options.muteState.slice(0,1)
+					fb.options.opt = fb.options.muteState.slice(1,2)
+
+				}
+				delete fb.options.muteState
 				changed = true
 			}
 		})
