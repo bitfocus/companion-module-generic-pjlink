f99d190d5355b545433c7e57327fa83f1d548266
John A Knight Jr <istnv@ayesti.com>
2023-02-20 12:37:23 -0800

    V3.0 update

--- upgrades.js
+++ upgrades.js
@@ -1,100 +1,116 @@
+import { CreateConvertToBooleanFeedbackUpgradeScript } from '@companion-module/base'
 
-module.exports = {
-	upgrade_choices: function (context, config, actions, feedbacks) {
-		let changed = false
-
-		if (config.pollTime == undefined || config.pollTime == '') {
-			config.pollTime = 10
-			changed = true
+export const UpgradeScripts = {
+	function(context, props) {
+		const result = {
+			updatedConfig: null,
+			updatedActions: [],
+			updatedFeedbacks: [],
 		}
 
-		actions.forEach((action) => {
-			switch (action.action) {
+		if (props.config) {
+			if (props.config.pollTime == undefined || '' == props.config.pollTime) {
+				props.config.pollTime = 10
+				result.updatedConfig = props.config
+			}
+		}
+
+		for (let action of props.actions) {
+			let changed = false
+			switch (action.actionId) {
 				case 'powerOn':
 					{
-						action.action = 'powerState'
+						action.actionId = 'powerState'
 						action.options.opt = '1'
 						changed = true
 					}
 					break
 				case 'powerOff':
 					{
-						action.action = 'powerState'
+						action.actionId = 'powerState'
 						action.options.opt = '0'
 						changed = true
 					}
 					break
 				case 'shutterOpen':
 					{
-						action.action = 'muteState'
+						action.actionId = 'muteState'
 						action.options.opt = '30'
 						changed = true
 					}
 					break
 				case 'shutterClose':
 					{
-						action.action = 'muteState'
+						action.actionId = 'muteState'
 						action.options.opt = '31'
 						changed = true
 					}
 					break
 				case 'freeze':
 					{
-						action.action = 'freezeState'
+						action.actionId = 'freezeState'
 						action.options.opt = '1'
 						changed = true
 					}
 					break
 				case 'unfreeze':
 					{
-						action.action = 'freezeState'
+						action.actionId = 'freezeState'
 						action.options.opt = '0'
 						changed = true
 					}
 					break
 			}
-		})
-		return changed // if something changed
+			if (changed) {
+				result.updatedActions.push(action)
+			}
+		}
+		return result
 	},
-	upgrade_muteaction: function (context, config, actions, feedbacks) {
-		let changed = false
 
-		actions.forEach((action) => {
-			// if ('muteState' == action.action && action.options.opt.slice(0,1)=='3') {
-			// 	action.options.opt = action.options.opt.slice(1,2)
-			// 	changed = true
-			// }
-		})
-		return changed
+	function (context,props) { // was broken in original upgradescipts
+		const result = {
+			updatedConfig: null,
+			updatedActions: [],
+			updatedFeedbacks: [],
+		}
+		return result
 	},
-	upgrade_muteaction2: function (context, config, actions, feedbacks) {
-		let changed = false
 
-		actions.forEach((action) => {
-			if ('muteState' == action.action) {
+	function (context, props) {
+		const result = {
+			updatedConfig: null,
+			updatedActions: [],
+			updatedFeedbacks: [],
+		}
+
+		for (let action of props.actions) {
+			if ('muteState' == action.actionId) {
 				if (action.options.opt.length == 2) {
-					action.options.opt = action.options.opt.slice(1,2)
-					action.options.item = action.options.opt.slice(0,1)
+					action.options.opt = action.options.opt.slice(1, 2)
+					action.options.item = action.options.opt.slice(0, 1)
 					changed = true
 				} else if (action.options.item == undefined) {
 					action.options.item = '3'
 					changed = true
 				}
 			}
-		})
+			if (changed) {
+				result.updatedActions.push(action)
+			}
+		}
 
 
-		feedbacks.forEach((fb) => {
-			if ('muteState' == fb.type) {
+		for (let fb of props.feedbacks) {
+			if ('muteState' == fb.feedbackId) {
 				if (fb.options.muteState) {
-					fb.options.item = fb.options.muteState.slice(0,1)
-					fb.options.opt = fb.options.muteState.slice(1,2)
-
+					fb.options.item = fb.options.muteState.slice(0, 1)
+					fb.options.opt = fb.options.muteState.slice(1, 2)
 				}
 				delete fb.options.muteState
-				changed = true
+				result.updatedFeedbacks.push(fb)
 			}
-		})
-		return changed
+		}
+		return result
 	}
 }
